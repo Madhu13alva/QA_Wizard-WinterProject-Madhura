d116ca10e1d9f1c5ef00e831d23528affd82ad8d
Madhura <madhurabalva@gmail.com>
2022-12-06 20:26:20 -0500

    All

--- src/test/java/runnerPkg/RunnerClass1.java
+++ src/test/java/runnerPkg/RunnerClass1.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features ="/Users/pkamerakodi/eclipse-workspace/WinterProjectQAWizard/src/test/java/feature",
+@CucumberOptions( features ="/Users/pkamerakodi/eclipse-workspace/WinterProjectQAWizard/src/test/java/featurePkg",
                   glue = "stepDefinition",
                   monochrome = true,
                   plugin = {"pretty","html:target/Cucumber_Report.html"}
